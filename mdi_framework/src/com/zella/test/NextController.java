7a497c60cbf574f5ddbe0dac729b16dec80c3cef
devrost2 <aikidospg>
2014-02-26 17:16:58 +0700

    add jfxtras and internal library

--- mdi_framework/src/com/zella/test/NextController.java
+++ mdi_framework/src/com/zella/test/NextController.java
@@ -3,16 +3,24 @@ package com.zella.test;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.zella.mdiframework.AbstractScreenController;
-import com.zella.mdiframework.IControlledScreen;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import jfxtras.labs.scene.control.window.CloseIcon;
+import jfxtras.labs.scene.control.window.MinimizeIcon;
+import jfxtras.labs.scene.control.window.Window;
+
+import com.zella.mdiframework.AbstractScreenController;
+import com.zella.mdiframework.IControlledScreen;
 
 public class NextController implements Initializable, IControlledScreen {
 
 	MyScreenController myController;
+
+	@FXML
+	private Pane pane;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -22,6 +30,24 @@ public class NextController implements Initializable, IControlledScreen {
 	@FXML
 	private void goToLogin(ActionEvent event) {
 		myController.gotoLogin();
+	}
+
+	@FXML
+	private void openInternalWindow(ActionEvent event) {
+
+		Window w = new Window("My MDI Window");
+		// define the initial window size
+		w.setPrefSize(300, 200);
+		// either to the left
+		w.getLeftIcons().add(new CloseIcon(w));
+		// .. or to the right
+		w.getRightIcons().add(new MinimizeIcon(w));
+		// add some content
+		w.getContentPane().getChildren()
+				.add(new Label("Content... \nof the window"));
+		w.getContentPane().getChildren().add(w);
+		// add the window to the canvas
+		pane.getChildren().add(w);
 	}
 
 	@Override
